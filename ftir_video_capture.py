12fd9f04276a523f9dc89da3d47f02342b21804e
alangreenbean <alan070516180339887@gmail.com>
2022-10-10 10:32:40 +0800

    Initial update

---
+++ ftir_video_capture.py
@@ -0,0 +1,111 @@
+import cv2
+import numpy as np
+import time
+from recognize import recognize_trace
+
+def nothing(x):
+    pass
+
+# Choose your webcam: 0, 1, ...
+cap = cv2.VideoCapture(1)
+if not cap.isOpened():
+	cap.open()
+
+cv2.namedWindow('Threshold Sliders')
+cv2.createTrackbar('R', 'Threshold Sliders', 50, 255, nothing)
+cv2.createTrackbar('B', 'Threshold Sliders', 50, 255, nothing)
+
+status = 0 # Not recognizing, status = 1 => recognizing.
+leave_times = 0 # The times that the finger leaves the pad.
+leave_time = 0 # The time that the finger leaves the pad.
+finger_pos = []   # A list to store the position of finger.
+origin_end = [] # A list to store the origin and the end of every sections.
+last_record = 0
+extreme = [0, 0, 1000, 1000]
+
+while(True):
+	# Get one frame from the camera
+	ret, frame = cap.read()
+	frame = cv2.flip(frame, 0)
+	cv2.imshow('frame', frame)
+
+	# Split RGB channels
+	b, g, r = cv2.split(frame)
+	zeros = np.zeros(frame.shape[:2], dtype="uint8")
+
+	# get the current value of the slider
+	r_thresh = cv2.getTrackbarPos('R', 'Threshold Sliders')
+	b_thresh = cv2.getTrackbarPos('B', 'Threshold Sliders')
+
+	# Perform thresholding to each channel
+	_, r_thresh_binary = cv2.threshold(r, r_thresh, 255, cv2.THRESH_BINARY)
+	_, b_thresh_binary_inv = cv2.threshold(b, b_thresh, 255, cv2.THRESH_BINARY_INV)
+
+	# Get the final result using bitwise operation
+	result = cv2.bitwise_and(r_thresh_binary, b_thresh_binary_inv, mask = None)
+#	result = cv2.blur(result, frame.shape[:2])
+
+	# Find and draw contours
+	contours, hierarchy = cv2.findContours(result, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	display = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
+	cv2.drawContours(display, contours, -1, (0,0,255))	
+	if len(contours) > 0:
+		if leave_time > 0:
+			leave_times += 1
+		leave_time = 0
+		if status == 0: # Start recognizing.
+			status = 1
+		x_main = 0
+		y_main = 0
+		rad_main = 0
+		for cnt in contours:
+			area = cv2.contourArea(cnt)
+			(x,y), radius = cv2.minEnclosingCircle(cnt)
+			if radius > rad_main:
+				rad_main = radius
+				x_main = x
+				y_main = y
+		x_main = int(x_main)
+		y_main = int(y_main)
+		rad_main = int(rad_main)
+
+		text = '(' + str(x_main) + ',' + str(y_main) + ')'
+		cv2.putText(display, text, (x_main, y_main),
+					cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
+		cv2.circle(display, (x_main, y_main), rad_main, (0, 255, 0))
+		if time.time() - last_record > 0.02:
+			finger_pos.append((x_main, y_main))
+			extreme[0] = max(x_main, extreme[0])
+			extreme[1] = max(y_main, extreme[1])
+			extreme[2] = min(x_main, extreme[2])
+			extreme[3] = min(y_main, extreme[3])
+	else:
+		if status == 1:
+			if leave_time == 0:
+				leave_time = time.time()
+				origin_end.append([finger_pos[0], finger_pos[-1]])
+				finger_pos = []
+			if time.time() - leave_time > 0.7:# The writer has finished writing.
+				number = recognize_trace(origin_end, extreme, leave_times)
+				origin_end = []
+				status = 0
+				finger_pos = []
+				leave_time = 0
+				leave_times = 0
+				print(number)
+
+	# Show the frame	
+	cv2.imshow("Red", cv2.merge([zeros, zeros, r_thresh_binary]))
+	cv2.imshow("Blue", cv2.merge([b_thresh_binary_inv, zeros, zeros]))
+	cv2.imshow("display", display)
+#	cv2.imshow('frame', result)
+
+	# Press q to quit
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# Release the camera
+cap.release()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
---
+++ recognize.py
@@ -0,0 +1,144 @@
+import cv2
+import numpy as np
+import math
+import time
+
+def recognize_trace(origin_end, extreme, stop_time):
+	x_max = extreme[0]
+	y_max = extreme[1]
+	x_min = extreme[2]
+	y_min = extreme[3]
+	if x_max == x_min:
+		x_max += 0.000000001
+	if y_max == y_min:
+		y_max += 0.000000001
+#	print(stop_time)
+#	print(origin_end)
+	for session in range(stop_time + 1):
+		for j in range(2):
+#			print(session)
+#			print(j)
+			origin_end[session][j] = ((origin_end[session][j][0] - x_min) / (x_max - x_min), 2 * (origin_end[session][j][1] - y_min) / (y_max - y_min))
+#	print(origin_end)
+	if stop_time > 0:
+		# The number is 4, 5, or 7
+		if origin_end[0][0][1] > 1.75 and origin_end[1][1][1] > 1.75:
+			return 5
+		elif origin_end[0][1][0] > 0.8:
+			return 4
+		else:
+			return 7
+	else:
+		# The number is 0, 1, 2, 3, 6, 8, or 9
+		if math.sqrt((origin_end[0][0][0] - origin_end[0][1][0])**2 + (origin_end[0][0][1] - origin_end[0][1][1])**2) < 0.3:
+			# The number is 0 or 8
+			if origin_end[0][1][0] > 0.75:
+				return 8
+			else:
+				return 0
+		else:
+			# The number is 1, 2, 3, 6, or 9
+			if origin_end[0][0][0] < 0.3 and origin_end[0][1][0] < 0.3:
+				return 3
+			elif origin_end[0][0][0] > 0.6 and origin_end[0][1][0] > 0.6:
+				return 9
+			else:
+				# The number is 1, 2, or 6
+				if origin_end[0][0][1] < 1.9:
+			   		return 2
+				if origin_end[0][1][1] < 0.1:
+					return 1
+				else:
+					return 6
+	return -1
+		
+#	size = len(finger_pos)
+#	finger_pos = normalize(finger_pos)
+#	vector = [finger_pos[idx] - finter_pos[(idx + 1) % size] for idx in range(size)]
+	# Count the number of straight line.
+	# We define that two vectors belong to a straight line
+	#		if the angle between them is less than 10 degree.
+#	straight_line = []
+#	switching_angle = []
+#	switch_dist = []
+#	for idx in range(size - 1):
+#		outer_product = vector[idx][0] * vector[idx + 1][1] - vector[idx][1] * vector[idx + 1][0]
+#		if outer_product > 0:
+#		 	switching_angle.append(1)
+#		else:
+#			switching_angle.append(0)
+#		switch_dist.append(sqrt((finger_pos[0][0] - finger_pos[idx][0])**2 + (finger_pos[0][1] - finger_pos[idx][1])**2))
+#		inner_product = vector[idx][0] * vector[idx + 1][0] + vector[idx][1] * vector[idx + 1][1]
+#		length_product = sqrt(vector[idx][0] ** 2 + vector[idx][1] ** 2) + sqrt(vector[idx + 1][0] ** 2 + vector[idx + 1][1] ** 2)
+#		angle = innter_product / length_product
+#		if angle > 0.98:
+#			straight_line.append(1)
+#		else:
+#			straight_line.append(0)
+#	straight_line_n = 0
+#	for i in range(len(straight_line) - 1):
+#		if straight_line[i] == straight_line[i + 1]:
+#			continue
+#		straight_line_n += 1
+#	switching_angle_n = 0
+#	for i in range(len(switching_angle) - 1):
+#		if switching_angle[i] == switching_angle[i + 1]:
+#			continue
+#		switching_angle_n += 1
+#	switch_dist_n = 0
+
+	
+	# start classifying
+#	if sqrt(vector[size - 2][0] ** 2 + vector[size - 2][1] ** 2) < 0.3:
+		# The number is 0 or 8
+#		if switching_angle_n > 0:
+#			return 8
+#		else:
+#			return 0
+#	else:
+		# 1, 2, 3, 4, 5, 6, 7, 9 remaining
+#		if finger_pos[0][1] < 1 and finger_pos[size - 1][1] < 1:
+			# The number is 5 or 6
+#			if stop_time > 0:
+#				return 5
+#			else:
+#				return 6
+#		else:
+			# 1, 2, 3, 4, 6, 7, 9 remaining
+#			if switching_angle_n == 0:
+				# The number is 1, 4, or 6.
+#				if switch_dist == 0:
+#					return 1
+#				elif switch_dist < 4:
+#					return 6
+#				else:
+#					return 4
+#			elif switching_angle_n == 1:
+				# The number is 2, 7, or 9.
+#				if finger_pos[0][0] > 0.5 and finger_pos[size - 1][0] > 0.5:
+#					return 9
+#				elif stop_time == 0:
+#				 		return 2
+#				else:
+#					return 7	
+#			else:
+#				return 3	
+#	return -1
+	
+
+
+
+if __name__ == "__main__":
+	oe1 = [[(1,3), (-2,-2)]]
+	ex1 = [2, 3, -2, -5]
+	st1 = 0
+	oe1 = [[(100,33), (102,31)]]
+	ex1 = [103, 34, 98, 30]
+	st1 = 0
+	oe1 = [[(0, 0), (1.5, -2)], [(1,0), (1,-4)]]
+	ex1 = [1.5, 0, -2, -4]
+	st1 = 1
+	print(recognize_trace(oe1, ex1, st1))	
+
+
+
